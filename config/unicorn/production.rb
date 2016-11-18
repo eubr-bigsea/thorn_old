6030078f248ce71aa1ad19d277c7d946dc0055e5
Guilherme Maluf <guimalufb@gmail.com>
2016-11-18 12:25:33 -0200

    Use relative app path on unicorn config
    Removes hardcoded app path on unicorn

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,8 +1,9 @@
-working_directory "/home/ec2-user/thorn/current"
+app_path = File.expand_path(File.dirname(__FILE__) + '/../..')
+working_directory "#{app_path}/current"
 worker_processes 4
 listen "/tmp/thorn.sock", backlog: 64
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 timeout 60
-pid "/home/ec2-user/thorn/shared/tmp/pids/unicorn.pid"
 stderr_path "log/unicorn.stderr.log"
 stdout_path "log/unicorn.stdout.log"
 
