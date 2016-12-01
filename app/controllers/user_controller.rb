ff10c97b4fc6146399f728eb4e2410e41f15f6de
Matheus Gonçalves <souzagonc@gmail.com>
2016-12-01 19:27:02 -0200

    change api endpoint

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,6 +2,6 @@ class UserController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    render json: @current_user.as_json
+    render json: User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/:id' => 'user#show'
   mount_devise_token_auth_for 'User', at: 'users', controllers: { passwords: 'devise_token_auth/passwords' }
-  get 'user' => 'user#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
