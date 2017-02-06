4e07040bcd36143f26ba7322854468b22295a96f
Matheus Gonçalves <souzagonc@gmail.com>
2017-02-06 17:23:46 -0200

    implement route to ask valid token of user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,16 @@
 class UserController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:show]
 
   def show
     render json: User.find(params[:id])
+  end
+
+  def validate_token
+    @user = User.find(params[:id])
+    if @user && @user.valid_token?(params['access-token'], params[:client])
+      render json: @user
+    else
+      render json: { message: 'token not valid' }, status: :unprocessable_entity
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'users/:id' => 'user#show'
+  post 'users/valid_token' => 'user#validate_token'
   mount_devise_token_auth_for 'User', at: 'users', controllers: { passwords: 'devise_token_auth/passwords' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
